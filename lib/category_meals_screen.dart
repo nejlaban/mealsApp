a0927345338653a30a4c543f8dcf555519c0def7
nejlaban <nejla.bandic@stu.ibu.edu.ba>
2021-07-06 09:18:35 +0200

    Add category_meals_screen

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('The recipes'),
+        ),
+        body: Center(
+          child: Text('The recišes for the category'),
+        ));
+  }
+}
